3b173da07f6e44f6a93a2916557756cc429fac3f
rolling-space <petros@rolling.space>
2017-01-05 23:47:26 +0100

    Better utilities

--- init.zsh
+++ init.zsh
@@ -91,13 +91,45 @@ elif (( ${+commands[curl]} )); then
   alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
 fi
 
+# Mac OS X Everywhere
+if [[ "$OSTYPE" == darwin* ]]; then
+  alias o='open'
+elif [[ "$OSTYPE" == cygwin* ]]; then
+  alias o='cygstart'
+  alias pbcopy='tee > /dev/clipboard'
+  alias pbpaste='cat /dev/clipboard'
+else
+  alias o='xdg-open'
 
+  if (( $+commands[xclip] )); then
+    alias pbcopy='xclip -selection clipboard -in'
+    alias pbpaste='xclip -selection clipboard -out'
+  elif (( $+commands[xsel] )); then
+    alias pbcopy='xsel --clipboard --input'
+    alias pbpaste='xsel --clipboard --output'
+  fi
+fi
+
+alias pbc='pbcopy'
+alias pbp='pbpaste'
 #
 # Resource Usage
 #
 
 alias df='df -kh'
 alias du='du -kh'
+
+if (( $+commands[htop] )); then
+  alias top=htop
+else
+  if [[ "$OSTYPE" == (darwin*|*bsd*) ]]; then
+    alias topc='top -o cpu'
+    alias topm='top -o vsize'
+  else
+    alias topc='top -o %CPU'
+    alias topm='top -o %MEM'
+  fi
+fi
 
 
 #
@@ -120,6 +152,41 @@ fi
 # Misc
 #
 
-mkcd() {
+# Makes a directory and changes to it.
+mkdcd() {
   [[ -n ${1} ]] && mkdir -p ${1} && builtin cd ${1}
+}
+
+#
+# Functions
+#
+
+# Changes to a directory and lists its contents.
+function cdls {
+  builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pushes an entry onto the directory stack and lists its contents.
+function pushdls {
+  builtin pushd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pops an entry off the directory stack and lists its contents.
+function popdls {
+  builtin popd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Prints columns 1 2 3 ... n.
+function slit {
+  awk "{ print ${(j:,:):-\$${^@}} }"
+}
+
+# Finds files and executes a command on them.
+function find-exec {
+  find . -type f -iname "*${1:-}*" -exec "${2:-file}" '{}' \;
+}
+
+# Displays user owned processes status.
+function psu {
+  ps -U "${1:-$LOGNAME}" -o 'pid,%cpu,%mem,command' "${(@)argv[2,-1]}"
 }
